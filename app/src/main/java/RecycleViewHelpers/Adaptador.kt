b7fb0f0b5fefe6fc8b0a8786021d0a4a0665789c
Josue <20230098@ricaldone.edu.sv>
2024-07-05 11:48:34 -0600

    Bueno ya avanzamos creo xd

--- app/src/main/java/RecycleViewHelpers/Adaptador.kt
+++ app/src/main/java/RecycleViewHelpers/Adaptador.kt
@@ -1,8 +1,27 @@
 package RecycleViewHelpers
 
-class Adaptador {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import josuehernandez.ethanhenriquez.hospitalbloomformativo.R
+import modelo.Doctores
 
-    //Necesitamos la base de datos
+class Adaptador(var Datos: List<Doctores>): RecyclerView.Adapter<ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //Unir el Recyclerview con la card
+        val vista = LayoutInflater.from(parent.context).inflate(R.layout.activity_item_card, parent, false)
+        return ViewHolder(vista)
+    }
+
+    override fun getItemCount() = Datos.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //Controlar la card
+        val item = Datos[position]
+        holder.txtNombreCard.text = item.Nombre
+    }
 
 
 }
