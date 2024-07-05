ef227ca082ed2ad24d0dbf8ef905d678967b416e
Josue <20230098@ricaldone.edu.sv>
2024-07-05 00:07:47 -0600

    por si acaso

--- app/src/main/java/josuehernandez/ethanhenriquez/hospitalbloomformativo/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/josuehernandez/ethanhenriquez/hospitalbloomformativo/ui/dashboard/DashboardFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import josuehernandez.ethanhenriquez.hospitalbloomformativo.databinding.FragmentDashboardBinding
+import kotlinx.coroutines.CoroutineScope
+import modelo.ClaseConexion
 
 class DashboardFragment : Fragment() {
 
@@ -31,6 +35,26 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
         }
         return root
+
+        //NO SE DONDE VA ESTO Xd
+
+        //1-Mandamos a llamar a todos los datos
+        //val txtnombrePaciente = findViewById<EditText>(R.id.txtnombrePaciente)
+        //val txtapellidoPaciente = findViewById<EditText>(R.id.txtapellidoPaciente)
+        //val txtenfermedadPaciente = findViewById<EditText>(R.id.txtenfermedadPaciente)
+        //val txtedadPaciente = findViewById<EditText>(R.id.txtedadPaciente)
+        //val txtnumHabitacion = findViewById<EditText>(R.id.txtnumHabitacion)
+        //val txtnumCama = findViewById<EditText>(R.id.txtnumCama)
+
+        //2-Programamos los botones
+        //btnagregar.setOnClickListener {
+        //  CoroutineScope(Dispatchers.IO).launch {
+                //1- Creamos un objeto de la base Conexion
+
+        //      val objConexion = ClaseConexion.cadenaConexion()
+        //    }
+        //}
+
     }
 
     override fun onDestroyView() {
