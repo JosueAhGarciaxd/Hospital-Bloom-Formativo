3e3948003d92c638bfabd559e81cd2ea3f9c42a9
ethan <20230006@ricaldone.edu.sv>
2024-07-24 21:37:49 -0600

    creo que asi se actualiza

--- app/src/main/java/RecycleViewHelpers/Adaptador.kt
+++ app/src/main/java/RecycleViewHelpers/Adaptador.kt
@@ -2,11 +2,13 @@ package RecycleViewHelpers
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import josuehernandez.ethanhenriquez.hospitalbloomformativo.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import modelo.ClaseConexion
 import modelo.Paciente
 
@@ -33,27 +35,92 @@ class Adaptador(var Datos: List<Paciente>): RecyclerView.Adapter<ViewHolder>() {
         holder.txtcardenfermedad.text = item.enfermedad.toString()
         holder.txtcarMedicamento.text = item.medicamento.toString()
 
-    }
 
-    fun paraEliminar(nombre: String, position: Int){
-        val ListaDatos = Datos.toMutableList()
-        ListaDatos.removeAt(position)
+        fun paraEliminar(nombre: String, position: Int){
+            val ListaDatos = Datos.toMutableList()
+            ListaDatos.removeAt(position)
 
-        GlobalScope.launch(Dispatchers.IO){
-            val objConexion = ClaseConexion().cadenaConexion()
+            GlobalScope.launch(Dispatchers.IO){
+                val objConexion = ClaseConexion().cadenaConexion()
 
-            //variable con el PrepareStatement
-            val deletePaciente = objConexion?.prepareStatement("delete from Paciente where nombre = ?")!!
-            deletePaciente.setString(1, nombre)
-            deletePaciente.executeUpdate()
+                //variable con el PrepareStatement
+                val deletePaciente = objConexion?.prepareStatement("delete from Paciente where nombre = ?")!!
+                deletePaciente.setString(1, nombre)
+                deletePaciente.executeUpdate()
 
-            val commit = objConexion.prepareStatement("commit")
-            commit.executeUpdate()
+                val commit = objConexion.prepareStatement("commit")
+                commit.executeUpdate()
+            }
+
+            Datos = ListaDatos.toList()
+            notifyItemRemoved(position)
+            notifyDataSetChanged()
         }
 
-        Datos = ListaDatos.toList()
-        notifyItemRemoved(position)
-        notifyDataSetChanged()
+        val paciente = Datos[position]
+        holder.txtCardNombre.text =paciente.nombre
+        holder.txtcardApellido.text = paciente.apellido
+        holder.txtcardedad.text = paciente.edad.toString()
+        holder.txtcardNumH.text = paciente.numHabitacion.toString()
+        holder.txtcardNumcama.text = paciente.numCama.toString()
+        holder.txtcardFechaIngreso.text = paciente.fechaIngreso
+        holder.txtcardenfermedad.text = paciente.enfermedad.toString()
+        holder.txtcarMedicamento.text = paciente.medicamento.toString()
+
+        holder.btnEliminar.setOnClickListener{
+            //alert
+            val context = holder.itemView.context
+
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Eliminar")
+            builder.setTitle("¿Desea eliminar los datos del paciente?")
+            //botones
+            builder.setPositiveButton("Si"){dialog, switch -> paraEliminar(paciente. nombre, position)
+
+            }
+
+            builder.setNegativeButton("No"){dialog, switch -> dialog.dismiss()
+
+            }
+            val dialog = builder.create()
+            dialog.show()
+        }
+
+        ////////////////////para eliminar ////////////////
+
+        fun paActualizar(nuevaLista: List<Paciente>){
+            Datos = nuevaLista
+            notifyDataSetChanged() /////////notificamos
+        }
+
+
+
+        fun actualizaciondePantalla(newNombre: String, newApellido: String, newEdad: Number, newNumHabitacion: Number, newNumCama: Number, newFechaIngreso: String, newEnfermedad: Number, newMedicamento: Number, uuid: String){
+            GlobalScope.launch(Dispatchers.IO) {
+                val objConexion = ClaseConexion().cadenaConexion()
+
+                val updatePaciente = objConexion?.prepareStatement("update Paciente set nombre = ?, apellido = ?, edad = ?, numHabitacion = ?, numCama = ?, fechaIngreso = ?, enfermedad = ?, medicamento = ? where uuid = ?")!!
+                updatePaciente.setString(1, newNombre)
+                updatePaciente.setString(2, newApellido)
+                updatePaciente.setInt(3, newEdad.toInt())
+                updatePaciente.setInt(4, newNumHabitacion.toInt())
+                updatePaciente.setInt(5, newNumCama.toInt())
+                updatePaciente.setString(6, newFechaIngreso)
+                updatePaciente.setInt(7, newEnfermedad.toInt())
+                updatePaciente.setInt(8, newMedicamento.toInt())
+                updatePaciente.setString(8, uuid)
+
+                updatePaciente.executeUpdate()
+                withContext(Dispatchers.Main){
+                    actualizaciondePantalla(newNombre, newApellido, newEdad, newNumHabitacion, newNumCama, newFechaIngreso, newEnfermedad, newMedicamento, uuid)
+                }
+
+
+
+            }
+        }
+
+
     }
 
 
